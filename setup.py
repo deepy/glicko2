51924965e36254957bde411ef020754b685986a5
Alex Nordlund <deep.alexander@gmail.com>
2023-08-27 21:59:13 +0200

    Drop overly old versions

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="glicko2",
-    version="2.0.0",
+    version="2.1.0",
     author="Alexander Nordlund",
     author_email="deep.alexander@gmail.com",
     description="Python implementation of glicko2",
@@ -14,10 +14,9 @@ setuptools.setup(
     url="https://github.com/deepy/glicko2",
     packages=setuptools.find_packages(),
     classifiers=[
-        "Programming Language :: Python :: 2",
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=2.7',
+    python_requires='>=3.7',
 )
